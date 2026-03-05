791ea45f1cd83c3077d97fc5818e2ef2e7c31c6b
Deepranjan Bharali <deepranjan_ug_23@ece.nits.ac.in>
2026-03-06 00:43:42 +0530

    change server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,9 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
+const __dirname = path.resolve();
 console.log(ENV.PORT);
 
 app.get("/health", (req, res) => {
@@ -18,17 +15,11 @@ app.get("/books", (req, res) => {
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-    const staticPath = path.join(__dirname, "../../frontend/dist");
-    console.log("NODE_ENV:", ENV.NODE_ENV);
-    console.log("__dirname:", __dirname);
-    console.log("Static path:", staticPath);
-    app.use(express.static(staticPath))
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname,"../../frontend/dist/index.html"))
     });
-} else {
-    console.log("Not in production mode, NODE_ENV:", ENV.NODE_ENV);
 }
 
 app.listen(ENV.PORT, () => {
