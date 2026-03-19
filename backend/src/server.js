a282314822951da5ac7a227519394a2d7f51c3ae
Deepranjan Bharali <deepranjan_ug_23@ece.nits.ac.in>
2026-03-20 00:08:45 +0530

    auth ansd db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
-import {connectDB} from "./lib/db.js"
+import {connectDB} from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express";
+
 
 const app = express();
 const __dirname = path.resolve();
 
-// Middleware
-// app.use(express.json());
+Middleware
+app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 // app.use(express.urlencoded({ extended: true }));
 
-
+app.use("/api/inngest",serve({client:inngest,functions}))
 // Routes
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "api is up running" });
